f566ac22218edf7abc2c8b4f51a2ffe26cca7bb2
osurfer3 <omar.alvarez@udc.es>
2014-04-14 11:16:27 +0200

    UI: Added tracker panel

--- create_dev_link.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /home/jamie/work/autoremove
-mkdir temp
-export PYTHONPATH=./temp
-python setup.py build develop --install-dir ./temp
-cp ./temp/AutoRemove.egg-link /home/jamie/.config/deluge/plugins
-rm -fr ./temp
