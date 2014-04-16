81fbf84be6640087fb7bc91439b9c418e50be823
omaralvarez <omar.alvarez@udc.es>
2014-04-16 20:53:54 +0200

    Version change

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ from setuptools import setup
 __plugin_name__ = "AutoRemovePlus"
 __author__ = "Omar Alvarez"
 __author_email__ = "osurfer3@hotmail.com"
-__version__ = "0.2"
+__version__ = "0.3"
 __url__ = "http://github.com/omaralvarez"
 __license__ = "GPLv3"
 __description__ = "Auto removal of torrents according to certain parameters"
