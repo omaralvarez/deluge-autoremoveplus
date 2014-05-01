ace7c21499bdd42fad1e13c3a1a766ce43b16622
omaralvarez <omar.alvarez@udc.es>
2014-05-02 00:07:13 +0200

    WebUI: Rearrange array comparison

--- autoremoveplus/data/autoremoveplus.js
+++ autoremoveplus/data/autoremoveplus.js
@@ -45,13 +45,11 @@ Deluge.plugins.autoremoveplus.MODULE_NAME = 'autoremoveplus';
 Deluge.plugins.autoremoveplus.DISPLAY_NAME = _('AutoRemovePlus');
 
 Deluge.plugins.autoremoveplus.util.arrayEquals = function(a, b) {
-    if (a.length != b.length) return false;
-    for (var i = 0; i < b.length; i++) {
-        //if (a[i].compare) { 
-        //    if (!a[i].compare(b[i])) return false;
-        //}
-        if (a[i] !== b[i]) return false;
-    }
+    if (a.length != b.length) 
+        return false;
+    for (var i = 0; i < b.length; i++)
+        if (a[i] !== b[i]) 
+            return false;
     return true;
 };
 
