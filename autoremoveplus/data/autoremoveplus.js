662b126d084cacb29fb234363642bf97522bfd20
omaralvarez <omar.alvarez@udc.es>
2014-05-01 23:55:32 +0200

    WebUI: Array comparison fix

--- autoremoveplus/data/autoremoveplus.js
+++ autoremoveplus/data/autoremoveplus.js
@@ -47,9 +47,9 @@ Deluge.plugins.autoremoveplus.DISPLAY_NAME = _('AutoRemovePlus');
 Deluge.plugins.autoremoveplus.util.arrayEquals = function(a, b) {
     if (a.length != b.length) return false;
     for (var i = 0; i < b.length; i++) {
-        if (a[i].compare) { 
-            if (!a[i].compare(b[i])) return false;
-        }
+        //if (a[i].compare) { 
+        //    if (!a[i].compare(b[i])) return false;
+        //}
         if (a[i] !== b[i]) return false;
     }
     return true;
