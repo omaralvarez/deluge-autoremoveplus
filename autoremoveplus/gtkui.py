9ab8882965ea81d6b7da3ef867ec6a9cea16983a
omaralvarez <omar.alvarez@udc.es>
2016-08-04 18:54:39 +0200

    GTKUI: Added new tabs to plugin to allow for tracker and label specific remove rules

--- autoremoveplus/gtkui.py
+++ autoremoveplus/gtkui.py
@@ -76,6 +76,22 @@ class GtkUI(GtkPluginBase):
         self.sel_func_store.append(["and"])
         self.sel_func_store.append(["or"])
 
+        # Buttons to add/delete rules
+        self._new_rule = self.glade.get_widget("new_rule")
+        self._new_rule.connect("clicked", self._do_new_rule)
+        self._delete_rule = self.glade.get_widget("delete_rule")
+        self._delete_rule.connect("clicked", self._do_delete_rule)
+
+        # Table to keep all rules
+        self._blk_rules = self.glade.get_widget("blk_rules")
+        self._view = self._build_view_rules()
+        window_rules = gtk.ScrolledWindow()
+        window_rules.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        window_rules.set_shadow_type(gtk.SHADOW_IN)
+        window_rules.add(self._view)
+        self._blk_rules.add(window_rules)
+        self._blk_rules.show_all()
+
         cell = gtk.CellRendererText()
 
         cbo_remove = self.glade.get_widget("cbo_remove")
@@ -99,11 +115,11 @@ class GtkUI(GtkPluginBase):
         self._delete_tracker.connect("clicked", self._do_delete_tracker)
 
         self._blk_trackers = self.glade.get_widget("blk_trackers")
-        self._view = self._build_view()
+        self._view_trackers = self._build_view_trackers()
         window = gtk.ScrolledWindow()
         window.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
         window.set_shadow_type(gtk.SHADOW_IN)
-        window.add(self._view)
+        window.add(self._view_trackers)
         self._blk_trackers.add(window)
         self._blk_trackers.show_all()
 
@@ -181,14 +197,28 @@ class GtkUI(GtkPluginBase):
     def on_click_enabled(self, check):
         self.disable_all_widgets(check.get_active())
 
+    def _do_new_rule(self,button):
+        new_row = self.lstore_rules.append(["Tracker", "New Tracker", "and", "Ratio", 0.0])
+        #self._view.set_cursor("3", start_editing=True)
+        path = self.lstore_rules.get_path(new_row)
+        self._view.set_cursor(path, focus_column=self._view.get_column(1), start_editing=True)
+
+    def _do_delete_rule(self,button):
+        selection = self._view.get_selection()
+        model, paths = selection.get_selected_rows()
+
+        for path in paths:
+            iter = model.get_iter(path)
+            model.remove(iter)
+
     def _do_new_tracker(self,button):
         new_row = self.lstore.append(["Tracker","New Tracker"])
         #self._view.set_cursor("3", start_editing=True)
         path = self.lstore.get_path(new_row)
-        self._view.set_cursor(path, focus_column=self._view.get_column(1), start_editing=True)
+        self._view_trackers.set_cursor(path, focus_column=self._view_trackers.get_column(1), start_editing=True)
 
     def _do_delete_tracker(self,button):
-        selection = self._view.get_selection()
+        selection = self._view_trackers.get_selection()
         model, paths = selection.get_selected_rows()
 
         for path in paths:
@@ -204,7 +234,7 @@ class GtkUI(GtkPluginBase):
         trackers = []
         labels = []
 
-        for row in self._view.get_model():
+        for row in self._view_trackers.get_model():
             if row[0] == "Tracker":
                 trackers.append(row[1])
             else:
@@ -288,8 +318,81 @@ class GtkUI(GtkPluginBase):
         else:
             self.glade.get_widget("cbo_sel_func").set_active(0)
 
-    def _build_view(self):
+    def _build_view_rules(self):
+        self.lstore_rules = gtk.ListStore(str, str, str, str, float)
+        view = gtk.TreeView(model=self.lstore_rules)
 
+        # Create field to set the type of rule tracker/label
+        liststore_field_type = gtk.ListStore(str)
+        for item in ["Tracker", "Label"]:
+            liststore_field_type.append([item])
+        crc = gtk.CellRendererCombo()
+        crc.set_property("editable", True)
+        crc.set_property("model", liststore_field_type)
+        crc.set_property("text-column", 0)
+        crc.set_property("has-entry", False)
+        crc.connect("edited", self._on_combo_type_changed)
+        # crc.set_active(0)
+        colc = gtk.TreeViewColumn(_("Type"), crc, text=0)
+        view.append_column(colc)
+
+        # Create text field for label or tracker names
+        crt = gtk.CellRendererText()
+        crt.set_property("editable", True)
+        crt.connect("edited", self._on_name_changed)
+        colt = gtk.TreeViewColumn(_("Name"), crt, text=1)
+        view.append_column(colt)
+
+        # Create field to set the type of selection and/or
+        liststore_field_logic = self.sel_func_store
+        crl = gtk.CellRendererCombo()
+        crl.set_property("editable", True)
+        crl.set_property("model", liststore_field_logic)
+        crl.set_property("text-column", 0)
+        crl.set_property("has-entry", False)
+        crl.connect("edited", self._on_combo_logic_changed)
+        #crl.set_active(0) #TODO
+        coll = gtk.TreeViewColumn(_("Operator"), crl, text=2)
+        view.append_column(coll)
+
+        # Create field for remove rule selection
+        liststore_field_rules = self.rules
+        crrr = gtk.CellRendererCombo()
+        crrr.set_property("editable", True)
+        crrr.set_property("model", liststore_field_rules)
+        crrr.set_property("text-column", 1)
+        crrr.set_property("has-entry", False)
+        crrr.connect("edited", self._on_combo_rules_changed)
+        colrr = gtk.TreeViewColumn(_("Remove Rule"), crrr, text=3)
+        view.append_column(colrr)
+
+        # Spin button for minimum value
+        crm = gtk.CellRendererSpin()
+        crm.set_property("editable", True)
+        crm.set_property("digits", 3)
+        crm.set_property("adjustment", gtk.Adjustment(0, 0, 10000.0, 0.5, 10,0))
+        crm.connect("edited", self._on_spin_min_changed)
+        colm = gtk.TreeViewColumn(_("Minimum"), crm, text=4)
+        view.append_column(colm)
+
+        return view
+
+    def _on_combo_type_changed(self, widget, path, text):
+        self.lstore_rules[path][0] = text
+
+    def _on_name_changed(self, widget, path, text):
+        self.lstore_rules[path][1] = text
+
+    def _on_combo_logic_changed(self, widget, path, text):
+        self.lstore_rules[path][2] = text
+
+    def _on_combo_rules_changed(self, widget, path, text):
+        self.lstore_rules[path][3] = text
+
+    def _on_spin_min_changed(self, widget, path, value):
+        self.lstore_rules[path][4] = float(value)
+
+    def _build_view_trackers(self):
         self.lstore = gtk.ListStore(str, str)
         view = gtk.TreeView(model=self.lstore)
 
